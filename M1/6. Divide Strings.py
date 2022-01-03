564595f5e8b474fdb79bb444c1b602af24a072e7
Arnav Shindolkar <51039549+arxxv@users.noreply.github.com>
2022-01-03 10:32:49 +0530

    Update 6. Divide Strings.py

--- M1/6. Divide Strings.py
+++ M1/6. Divide Strings.py
@@ -6,8 +6,7 @@ class Solution:
             return self.gcdOfStrings(str2, str1)
         gcd = math.gcd(len(str1), len(str2))
         if str1+str2 != str2+str1:
-            return ''
-        
+            return ''        
         return str1[:gcd]
 
 sol = Solution()
