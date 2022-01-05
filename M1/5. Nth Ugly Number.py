76a295b05c2f9c929395807ad1ff716f856e55c2
Arnav Shindolkar <51039549+arxxv@users.noreply.github.com>
2022-01-05 10:09:57 +0530

    Update 5. Nth Ugly Number.py

--- M1/5. Nth Ugly Number.py
+++ M1/5. Nth Ugly Number.py
@@ -4,17 +4,17 @@ class Solution:
         dp[0] = 1
         c = 1
         
-        i2,i3,i5=0,0,0
+        i2, i3, i5 = 0, 0, 0
         
         while c < n:
-            n2, n3, n5 = dp[i2]*2, dp[i3]*3, dp[i5]*5
+            nxt2, nxt3, nxt5 = dp[i2]*2, dp[i3]*3, dp[i5]*5
             
-            x = min(n2,n3,n5)
-            dp[c] = x
+            nxt = min(nxt2,nxt3,nxt5)
+            dp[c] = nxt
             c+=1
             
-            if n2 == x: i2+=1
-            if n3 == x: i3+=1
-            if n5 == x: i5+=1
+            if nxt2 == nxt: i2+=1
+            if nxt3 == nxt: i3+=1
+            if nxt5 == nxt: i5+=1
         
         return dp[n-1]
